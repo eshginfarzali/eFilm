cafb98003555aba9c50f794d3dd2847a55f7e5ea
eshginfarzali <85007480+eshginfarzali@users.noreply.github.com>
2023-02-10 22:40:58 +0400

    fix-bug

--- assets/app.js
+++ assets/app.js
@@ -1,16 +1,45 @@
 let btn =document.getElementsByClassName("ticket__buy-btn")
 let price = document.getElementById("price")
+let store = document.getElementById("store")
+let basket=document.getElementById("basket")
+let count=document.getElementById("count")
 
-
-if(JSON.parse(localStorage.getItem('arr')===null)){
-    localStorage.setItem("arr",JSON.stringify([]))
-}
-else{
-   let arr =JSON.parse(localStorage.getItem("arr"))
-}
-
-btn.addEventListener('click',(e)=>{
-    let arr =JSON.parse(localStorage.getItem("arr"))
-    localStorage.setItem("arr", JSON.stringify([...arr, inp.value]))
-
+basket.addEventListener("click",()=>{
+    store.style.display === "none" ?
+        store.style.display = "flex":
+        store.style.display = "none"
 })
+
+
+if(JSON.parse(localStorage.getItem('cartsCount'))===null){
+    localStorage.setItem('basket',JSON.stringify([]))
+}
+
+for(let i=0; i<btn.length; i++){
+    btn[i].addEventListener('click',()=>{
+        cartCount()
+
+    })
+    
+}
+
+function cartCount(){
+   let prdCount= localStorage.getItem("cartsCount");
+    prdCount=parseInt(prdCount);
+    if(prdCount){
+        localStorage.setItem('cartsCount', prdCount+1)
+        count.textContent=prdCount+1;
+    }else{
+        localStorage.setItem('cartsCount',1);
+        count.textContent=prdCount=1;
+
+    }
+}
+
+function displayCart(){
+    let prdCount=localStorage.getItem('cartsCount');
+    if(prdCount){
+        count.textContent=prdCount;
+    }
+}
+displayCart();
