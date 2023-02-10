cb79c975a7b482c47f278f2c0ebf03381fc82cfe
eshginfarzali <85007480+eshginfarzali@users.noreply.github.com>
2023-02-10 20:11:15 +0400

    fix-bug

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,16 @@
-// setTimeout(setInterval(function(){
-//     console.log('salam')
-// },1000),5000)
+let btn =document.getElementsByClassName("ticket__buy-btn")
+let price = document.getElementById("price")
+
+
+if(JSON.parse(localStorage.getItem('arr')===null)){
+    localStorage.setItem("arr",JSON.stringify([]))
+}
+else{
+   let arr =JSON.parse(localStorage.getItem("arr"))
+}
+
+btn.addEventListener('click',(e)=>{
+    let arr =JSON.parse(localStorage.getItem("arr"))
+    localStorage.setItem("arr", JSON.stringify([...arr, inp.value]))
+
+})
